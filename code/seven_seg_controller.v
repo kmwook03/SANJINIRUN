94f9c7c9d16f78e00a8781f23a7d83786a7836d4
Jeongwon Park <105582726+no1pjw@users.noreply.github.com>
2025-11-29 11:55:03 +0900

    Update seven_seg_controller.v

--- code/seven_seg_controller.v
+++ code/seven_seg_controller.v
@@ -46,27 +46,24 @@ module seven_seg_controller (
     end
 
     wire [2:0] digit_idx = scan_cnt[16:14];
-    
     always @(*) begin
         // 제안서에 따르면 8 Array 7-Segment를 사용합니다.
         // 현재는 첫 번째 자리(맨 오른쪽)만 켜도록 설정 (Active Low: 0이 켜짐)
          for(i=0; i<8; i=i+1) seg_buffer[i] = 7'b0000000;
-
+        
         if (current_state == S_COUNTDOWN) begin
             // 0번(맨 오른쪽) 자리에 카운트다운 값 넣기
             seg_buffer[0] = hex2seg(countdown_val);
-            
         end else if (current_state == S_RUN) begin
             // 플레이 시간 자릿수별로 쪼개서 넣기
             // seg_buffer[0] : 1의 자리
             // seg_buffer[1] : 10의 자리
             // seg_buffer[2] : 100의 자리 ...
-            seg_buffer[0] = hex2seg(play_time % 10);
-            seg_buffer[1] = hex2seg((play_time / 10) % 10);
-            seg_buffer[2] = hex2seg((play_time / 100) % 10);
-            seg_buffer[3] = hex2seg((play_time / 1000) % 10);
+            seg_buffer[7] = hex2seg(play_time % 10);
+            seg_buffer[6] = hex2seg((play_time / 10) % 10);
+            seg_buffer[5] = hex2seg((play_time / 100) % 10);
+            seg_buffer[4] = hex2seg((play_time / 1000) % 10);
         end
-        
         if (current_state != S_COUNTDOWN && current_state != S_RUN) begin
             seg_out = 8'b00000000;
             seg_en  = 8'b00000000;
@@ -75,15 +72,15 @@ module seven_seg_controller (
             seg_out = {1'b0, seg_buffer[digit_idx]};
         
             case (digit_idx)
-                3'd0: seg_en = 8'b00000001;
-                3'd1: seg_en = 8'b00000010;
-                3'd2: seg_en = 8'b00000100;
-                3'd3: seg_en = 8'b00001000;
-                3'd4: seg_en = 8'b00010000;
-                3'd5: seg_en = 8'b00100000;
-                3'd6: seg_en = 8'b01000000;
-                3'd7: seg_en = 8'b10000000;
-                default: seg_en = 8'b00000000;
+                3'd0: seg_en = ~8'b00000001;
+                3'd1: seg_en = ~8'b00000010;
+                3'd2: seg_en = ~8'b00000100;
+                3'd3: seg_en = ~8'b00001000;
+                3'd4: seg_en = ~8'b00010000;
+                3'd5: seg_en = ~8'b00100000;
+                3'd6: seg_en = ~8'b01000000;
+                3'd7: seg_en = ~8'b10000000;
+                default: seg_en = ~8'b00000000;
             endcase
         end
     end            
