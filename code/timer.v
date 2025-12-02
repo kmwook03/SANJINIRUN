f05648f331f9bab417c8091c9d1101ca00a47fbe
MinWook <114015128+kmwook03@users.noreply.github.com>
2025-12-02 18:32:09 +0900

    Update timer.v

--- code/timer.v
+++ code/timer.v
@@ -5,12 +5,14 @@ module timer (
     
     output reg [3:0] countdown_val, // 3, 2, 1 표시용
     output reg countdown_done,      // 카운트다운 종료 신호
-    output reg stage,
+    output reg [2:0] stage,
     output reg [15:0] play_time     // 게임 플레이 시간 (초 단위)
 );
-    localparam S_COUNTDOWN = 2'b01;
+    
     localparam S_IDLE = 2'b00;
+    localparam S_COUNTDOWN = 2'b01;
     localparam S_RUN  = 2'b10;
+    localparam S_OVER = 2'b11;
 
     parameter DELAY = 50_000_000;
 
@@ -38,29 +40,34 @@ module timer (
             countdown_val <= 3;
             countdown_done <= 0;
             play_time <= 0;
+            stage <= 1;
         end else begin
-            if (current_state == S_COUNTDOWN) begin
-                if (tick_1s) begin
-                    if (countdown_val > 1) 
-                        countdown_val <= countdown_val - 1;
-                    else begin
-                        countdown_val <= 0;
-                        countdown_done <= 1; // 0이 되면 완료 신호 [cite: 76]
+            case (current_state)
+                S_COUNTDOWN: begin
+                    if (tick_1s) begin
+                        if (countdown_val > 1) 
+                            countdown_val <= countdown_val - 1;
+                        else begin
+                            countdown_val <= 0;
+                            countdown_done <= 1; // 0이 되면 완료 신호 [cite: 76]
+                        end
                     end
                 end
-            end else if (current_state == S_RUN) begin
-                countdown_done <= 0; // 리셋
-                if(stage == 0)
-                    stage = 1;
-                else if(play_time / 60 + 1 > stage)
-                    stage = stage + 1;
-                if (tick_1s) 
-                    play_time <= play_time + 1; // [cite: 81] 카운트업
-            end else begin
+                S_RUN: begin
+                    countdown_done <= 0; // 리셋
+                    if(stage == 0)
+                        stage = 1;
+                    else if(play_time / 60 + 1 > stage)
+                        stage = stage + 1;
+                    if (tick_1s) 
+                        play_time <= play_time + 1; // [cite: 81] 카운트업
+                end
+                default: begin
                 // IDLE 등에서는 초기값 유지
-                countdown_val <= 3;
-                play_time <= 0;
-            end
+                    countdown_val <= 3;
+                    play_time <= 0;
+                end
+            endcase
         end
     end
 endmodule
